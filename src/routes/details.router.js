8df2599b169fbe626ba9335355ac67812e84b248
KaiPow1206 <nguyennhoanhthai@gmail.com>
2024-11-08 23:27:41 +0700

    all done

--- src/routes/details.router.js
+++ src/routes/details.router.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { detailsAuthor, detailsComment, savePicture } from '../controllers/details.controller.js';
+import { middlewareToken } from '../config/jwt.js';
 
 const detailsRoutes = express.Router();
 
 detailsRoutes.get("/details-picture-author/:pictureID",detailsAuthor);
 detailsRoutes.get("/details-picture-comment/:pictureID",detailsComment);
-detailsRoutes.get("/save-picture/:pictureID",savePicture);
+detailsRoutes.get("/save-picture/:pictureID",middlewareToken,savePicture);
 export default detailsRoutes;
